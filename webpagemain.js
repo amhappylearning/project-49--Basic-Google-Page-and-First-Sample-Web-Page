370dc91e26d0a1d1852a8501b4cbcbb2aa2b3aa5
AM <73906893+amhappylearning@users.noreply.github.com>
2021-04-27 16:04:22 +0530

    Add files via upload

---
+++ webpagemain.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    document.getElementById("nameheading").style.color="green";
+    }
+    function changecolorwhite(){
+    document.getElementById("para").style.color="white";
+    }
+    //add more functions//
